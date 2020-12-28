9ef4346c68c5506b33fbffb94553745787884703
Lada <ladu3112@yandex.ru>
2020-12-28 14:29:53 +0300

    Connecting styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
-
+import './App.css';
 
 const App = () => {
   const items = [
@@ -16,10 +16,10 @@ const App = () => {
       value: 'Почитать дополнительные статьи'
     }
   ];
-  
+
   return(
-  <div>
-   <h1>Важные дела:</h1>
+  <div className="wrap">
+   <h1 className="wrap__title">Важные дела:</h1>
     <InputItem />
     <ItemList items={items}/>
    <Footer count={3}/>
