e2fea11f20b8e76244ce3c9eaeba54a11d191bca
Lada <ladu3112@yandex.ru>
2020-12-30 16:30:13 +0300

    Styling app

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = ({items}) => (<ul>
-  {items.map(item => <li key={item.value}>
+
+const ItemList = ({items}) => (<div>
+  {items.map(item => <div key={item.value}>
     <Item value={item.value} isDone={item.isDone}/>
-  </li>)}
-</ul>);
+  </div>)}
+</div>);
 
 export default ItemList;
