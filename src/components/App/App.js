45ba599a2786a9dc8d4b32d6dc132cae027a54a4
Lada <ladu3112@yandex.ru>
2021-01-03 18:18:09 +0300

    react component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,43 +9,44 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Посмотреть 5 уроков',
-      isDone: true
-    },
-    {
-      value: 'Выполнить домашние задания',
-      isDone: false
-    },
-    {
-      value: 'Почитать дополнительные статьи',
-      isDone: false
-    }
-  ];
-
-  const count = items.length;
-
-
-  return(
-  <div className={styles.wrap}>
-   <h1 className={styles.title}>Важные дела:</h1>
-   <FormControl className={styles.filter}>
-      <InputLabel id="demo-simple-select-label">Фильтр</InputLabel>
-      <Select>
-        <MenuItem>Все</MenuItem>
-        <MenuItem>Активные</MenuItem>
-        <MenuItem>Выполненные</MenuItem>
-      </Select>
-    </FormControl>
-    <ItemList items={items}/>
-    <InputItem />
-    <Footer count={count}/>
-    <Button className={styles.button} variant="contained" color="secondary">
-      Удалить выполненные дела
-    </Button>
-  </div>);
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Посмотреть 5 уроков',
+        isDone: true
+      },
+      {
+        value: 'Выполнить домашние задания',
+        isDone: false
+      },
+      {
+        value: 'Почитать дополнительные статьи',
+        isDone: false
+      }
+    ];
+  
+    const count = items.length;
+  
+    return(
+    <div className={styles.wrap}>
+     <h1 className={styles.title}>Важные дела:</h1>
+     <FormControl className={styles.filter}>
+        <InputLabel id="demo-simple-select-label">Фильтр</InputLabel>
+        <Select>
+          <MenuItem>Все</MenuItem>
+          <MenuItem>Активные</MenuItem>
+          <MenuItem>Выполненные</MenuItem>
+        </Select>
+      </FormControl>
+      <ItemList items={items}/>
+      <InputItem />
+      <Footer count={count}/>
+      <Button className={styles.button} variant="contained" color="secondary">
+        Удалить выполненные дела
+      </Button>
+    </div>);
+  }
 }
 
 export default App;
